0a711152dbc88d30952dfc8213e117f29a9d2f28
emanueltesfa <emanueltesfa0@gmail.com>
2022-04-11 01:12:37 -0400

    Working Final and Screenshots

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import numpy as np
 
 if __name__ == '__main__':
     x = 5
-    y = 4
+    y = 3
     max = np.array([
-        [0, 0, 1, 2], [1, 7, 5, 0], [2, 3, 5, 6], [0, 6, 5, 2], [0, 6, 5, 6]
+        [7, 5, 3], [3, 2, 2], [9, 0, 2], [2, 2, 2], [4, 3, 3]
     ])
     allocation = np.array([
-        [0, 0, 1, 2], [1, 0, 0, 0], [1, 3, 5, 4], [0, 6, 3, 2], [0, 0, 1, 4]
+        [0, 1, 0], [2, 0, 0], [3, 0, 2], [2, 1, 1], [0, 0, 2]
     ])
-
     need = max - allocation
-    available = np.array([1, 5, 2, 0])
-    failed = np.array([])
+    available = np.array([3, 0, 1])
+    failed = []
     rows = np.array([])
 
     for i in range(x):
@@ -22,26 +21,25 @@ if __name__ == '__main__':
             if available[j] >= need[i][j]:
                 count += 1
         if count == y:
-
-            rows = np.append(rows, i) # add row number to the safe sequence
-            available = available + allocation[i] # calc new available
+            rows = np.append(rows, i)  # add row number to the safe sequence
+            available = available + allocation[i]  # calc new available
         else:
             # print("false")
-            failed = [i]
+            failed.append(i)
             continue
 
-    count = 0
+    # checks back for failed sequences to see if new available is larger
     for a in range(len(failed)):
-        for b in range(i):
-            if available[b] >= need[failed][0][b]: # the available is now bigger"
+        count = 0
+        for b in range(y):
+            if available[b] >= need[failed][0][b]:  # the available is now bigger"
                 count += 1
         if count == y:
-            rows = np.append(rows, failed)
-            print("Safe sequence found", rows)
+            available = available + allocation[failed[0]]  # calc new available
+            rows = np.append(rows, failed[0])
         else:
-            print(count, y)
             print("No safe sequence found")
-            #     print("Failed", need[failed])
-        failed.pop()
-
+        if len(failed) > 1:
+            failed.pop(a)
+    print("Safe sequence found", rows)
     print("Available array is: \n", available, "\nNeed matrix is: \n", need)
